8b74eabe68a849fe7df20d91a067f6277ae477a9
Amitp0070 <amit7081483564@gmail.com>
2024-07-28 15:27:08 +0530

    Infinite Scroll and Pagination

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
+from django.core.paginator import Paginator
+from django.conf import settings
 from tracker.models import Transaction
 from tracker.filters import TransactionFilter
 from tracker.forms import TransactionForm
@@ -17,9 +19,13 @@ def transactions_list(request):
         request.GET,
         queryset=Transaction.objects.filter(user=request.user).select_related('category')
     )
+    paginator = Paginator(transaction_filter.qs, settings.PAGE_SIZE)
+    transaction_page = paginator.page(1) # default to 1 when this view in triggered
+
     total_income = transaction_filter.qs.get_total_income()
     total_expenses = transaction_filter.qs.get_total_expenses()
     context = {
+        'transactions': transaction_page,
         'filter': transaction_filter,
         'total_income': total_income,
         'total_expenses': total_expenses,
@@ -84,3 +90,16 @@ def delete_transaction(request, pk):
     }
     return render(request, 'tracker/partials/transaction-success.html', context)
  
+
+@login_required
+def get_transactions(request):
+    page = request.GET.get('page', 1)  #?page=2
+    transaction_filter = TransactionFilter(
+        request.GET,
+        queryset=Transaction.objects.filter(user=request.user).select_related('category')
+    )
+    paginator = Paginator(transaction_filter.qs, settings.PAGE_SIZE)
+    context = {
+        'transactions': paginator.page(page)
+    }
+    return render(request, 'tracker/partials/transactions-container.html#transaction_list', context)
